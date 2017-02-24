6c346d98fc99b5abfd44bb812893feabe383233a
hugotox <hpineda83@gmail.com>
2017-02-23 22:59:38 -0300

    add static setting

--- causas/settings.py
+++ causas/settings.py
@@ -15,6 +15,12 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/static/'
+ADMIN_MEDIA_PREFIX = '/media/'
+STATIC_URL = ''
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
