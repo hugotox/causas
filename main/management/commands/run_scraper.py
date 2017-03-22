72948bd1c7cf60bfe2d6f7136156930f15352950
hugotox <hpineda83@gmail.com>
2017-03-22 18:43:04 -0300

    Run infinitely when debug false

--- main/management/commands/run_scraper.py
+++ main/management/commands/run_scraper.py
@@ -1,8 +1,13 @@
 from django.core.management import BaseCommand
+from django.conf import settings
 
 from main.tasks import run_scraper
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        run_scraper()
+        if settings.DEBUG:
+            run_scraper()
+        else:
+            while True:
+                run_scraper()
