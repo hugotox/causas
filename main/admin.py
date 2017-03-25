256367216ced2ae9067051dbdcccfb28d3e63af4
hugotox <hpineda83@gmail.com>
2017-03-25 09:06:06 -0300

    Added admin filters

--- main/admin.py
+++ main/admin.py
@@ -4,23 +4,45 @@ from main.models import UserProfile, Causa, DocFamilia, DocCobranza, DocCivil, D
     DocSuprema
 
 
-class AdminCausa(admin.ModelAdmin):
-    list_filter = ('type',)
+class AdminDocSuprema(admin.ModelAdmin):
+    search_fields = ('id', 'nomenclatura', 'descripcion')
 
-    class Meta:
-        model = Causa
+
+class AdminDocApelaciones(admin.ModelAdmin):
+    search_fields = ('id', 'descripcion')
+
+
+class AdminDocCivil(admin.ModelAdmin):
+    search_fields = ('id', 'tramite', 'descripcion')
 
 
 class AdminDocLaboral(admin.ModelAdmin):
-    search_fields = ('tramite',)
+    search_fields = ('id', 'tramite',)
+
+
+class AdminDocPenal(admin.ModelAdmin):
+    search_fields = ('id', 'observacion',)
+
+
+class AdminDocCobranza(admin.ModelAdmin):
+    search_fields = ('id', 'tramite',)
+
+
+class AdminDocFamilia(admin.ModelAdmin):
+    search_fields = ('id', 'tramite',)
+
+
+class AdminCausa(admin.ModelAdmin):
+    list_filter = ('type',)
+    search_fields = ('id', 'caratulado')
 
 
 admin.site.register(UserProfile)
 admin.site.register(Causa, AdminCausa)
-admin.site.register(DocApelaciones)
-admin.site.register(DocSuprema)
+admin.site.register(DocApelaciones, AdminDocApelaciones)
+admin.site.register(DocSuprema, AdminDocSuprema)
 admin.site.register(DocLaboral, AdminDocLaboral)
-admin.site.register(DocPenal)
-admin.site.register(DocCivil)
-admin.site.register(DocCobranza)
-admin.site.register(DocFamilia)
+admin.site.register(DocPenal, AdminDocPenal)
+admin.site.register(DocCivil, AdminDocCivil)
+admin.site.register(DocCobranza, AdminDocCobranza)
+admin.site.register(DocFamilia, AdminDocFamilia)
